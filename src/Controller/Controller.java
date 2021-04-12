8281b91fd1c4be6afc97e700baee55aa8052ef00
DANIEL <daniel.tabares1@udea.edu.co>
2021-04-12 18:32:14 -0500

    se inicia la creación de interfaz

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,4 +1,16 @@
 package Controller;
 
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    public void facil_action(){
+        int filas, columnas, minas;
+        filas=8;
+        columnas=8;
+        minas=10;
+        System.out.println("facil");
+    }
+
+
 }

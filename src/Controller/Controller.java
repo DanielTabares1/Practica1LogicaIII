e21b92728b6ae8276940e1b995a1d47788c9841f
DANIEL <daniel.tabares1@udea.edu.co>
2021-04-14 16:34:22 -0500

    Se mejora la Interfaz, se crean los botones con todos los tamaños de juego, se establece diseño de los botones, se crean funciones para acceder a botones por indice y coordenadas

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,15 +1,75 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
+    int filas, columnas, minas;
 
-    public void facil_action(){
-        int filas, columnas, minas;
-        filas=8;
-        columnas=8;
-        minas=10;
-        System.out.println("facil");
+    @FXML
+    private TextField filas_id;
+    @FXML
+    private TextField columnas_id;
+    @FXML
+    private TextField minas_id;
+    @FXML
+    private Button dimensionesButton;
+
+    public void facil_action() throws IOException {
+        activarBotones(false);
+        filas = 8;
+        columnas = 8;
+        minas = 10;
+        lanzarJuego(new Stage());
+    }
+
+    public void medio_action() throws IOException {
+        activarBotones(false);
+        filas = 16;
+        columnas = 16;
+        minas = 40;
+        lanzarJuego(new Stage());
+    }
+
+    public void dificil_action() throws IOException {
+        activarBotones(false);
+        filas = 16;
+        columnas = 30;
+        minas = 99;
+        lanzarJuego(new Stage());
+    }
+
+    public void personalizado_action() {
+        activarBotones(true);
+    }
+
+    public void activarBotones(boolean b) {
+        filas_id.setDisable(!b);
+        columnas_id.setDisable(!b);
+        dimensionesButton.setDisable(!b);
+    }
+
+    public void iniciar_action() throws IOException {
+        System.out.println("acción");
+        filas = Integer.parseInt(filas_id.getText());
+        columnas = Integer.parseInt(columnas_id.getText());
+        minas = Integer.parseInt(minas_id.getText());
+        lanzarJuego(new Stage());
+    }
+
+    public void lanzarJuego(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../View/juego.fxml"));
+        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.setTitle("Jueguito");
+        primaryStage.show();
+        juegoController.setValores(filas,columnas,minas);
     }
 
 
--- src/Controller/juegoController.java
+++ src/Controller/juegoController.java
@@ -1,4 +1,65 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+import java.awt.*;
+
+
 public class juegoController {
+
+    @FXML
+    private Pane panelPrincipal;
+
+    public double hPanel, wPanel, hBoton, wBoton;
+    public static int minas, filas, columnas;
+
+    public void comenzar_action() {
+        hPanel = panelPrincipal.getHeight();
+        wPanel = panelPrincipal.getWidth();
+
+        hBoton = Math.floor(hPanel / filas);
+        wBoton = Math.floor(wPanel / columnas);
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                Button b = new Button();
+                b.setLayoutY(i * hBoton);
+                b.setLayoutX(j * wBoton);
+                b.setPrefWidth(wBoton);
+                b.setPrefHeight(hBoton);
+                b.setStyle("-fx-background-color: lightskyblue;" +
+                        "-fx-border-color: black");
+                panelPrincipal.getChildren().add(b);
+                b.setOnAction(actionEvent -> {
+                    int id = panelPrincipal.getChildren().indexOf(b);
+                    click(id);
+                });
+            }
+        }
+    }
+
+    public static void setValores(int f, int c, int m) {
+        filas = f;
+        columnas = c;
+        minas = m;
+    }
+
+    public void click(int id) {
+        int f = id / columnas + 1;
+        int c = id % columnas + 1;
+
+
+
+    }
+
+    public  void disenoBoton(Button b){
+        b.setDisable(true);
+        b.setStyle("-fx-background-color: indianred;" +
+                "-fx-border-color: black");
+    }
+
 }
